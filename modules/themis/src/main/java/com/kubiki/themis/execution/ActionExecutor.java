b1aa779703a79b2ebbb4e1d5d2868dcd0e2be6b2
Zajdel Patryk <57092803+xMOROx@users.noreply.github.com>
2026-05-05 10:16:21 +0200

    clean(themis): remove deprecated classes

--- modules/themis/src/main/java/com/kubiki/themis/execution/ActionExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.kubiki.themis.execution;
-
-public interface ActionExecutor {
-    boolean execute(String targetId);
-    boolean compensate(String targetId);
-    String getActionType();
-}
