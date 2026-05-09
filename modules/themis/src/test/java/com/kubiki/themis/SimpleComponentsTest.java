c3aad6ec771d8a82e68623bd3eabb14d4cf8fdbd
Zajdel Patryk <57092803+xMOROx@users.noreply.github.com>
2026-05-09 11:25:41 +0200

    test: refactor SimpleComponentsTest to use JUnit 5 features

--- modules/themis/src/test/java/com/kubiki/themis/SimpleComponentsTest.java
+++ modules/themis/src/test/java/com/kubiki/themis/SimpleComponentsTest.java
@@ -4,70 +4,101 @@ import com.kubiki.themis.config.ThemisProperties;
 import com.kubiki.themis.constants.OntologyConstants;
 import com.kubiki.themis.model.ExecutionStatus;
 import com.kubiki.themis.model.Protocol;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.lang.reflect.Constructor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Simple Components Tests")
 class SimpleComponentsTest {
 
-    @Test
-    void testProtocolEnum() {
-        assertEquals(3, Protocol.values().length);
-        assertEquals(Protocol.REST, Protocol.valueOf("REST"));
-        assertEquals(Protocol.SHELL, Protocol.valueOf("SHELL"));
-        assertEquals(Protocol.GRPC, Protocol.valueOf("GRPC"));
+    @ParameterizedTest
+    @EnumSource(Protocol.class)
+    @DisplayName("Protocol enum values should be valid")
+    void testProtocolEnum(Protocol protocol) {
+        assertAll(
+                () -> assertEquals(3, Protocol.values().length),
+                () -> assertEquals(protocol, Protocol.valueOf(protocol.name()))
+        );
+    }
+
+    @ParameterizedTest
+    @EnumSource(ExecutionStatus.class)
+    @DisplayName("ExecutionStatus enum values should be valid")
+    void testExecutionStatusEnum(ExecutionStatus status) {
+        assertAll(
+                () -> assertEquals(3, ExecutionStatus.values().length),
+                () -> assertEquals(status, ExecutionStatus.valueOf(status.name()))
+        );
     }
 
     @Test
-    void testExecutionStatusEnum() {
-        assertEquals(3, ExecutionStatus.values().length);
-        assertEquals(ExecutionStatus.IN_PROGRESS, ExecutionStatus.valueOf("IN_PROGRESS"));
-        assertEquals(ExecutionStatus.SUCCESS, ExecutionStatus.valueOf("SUCCESS"));
-        assertEquals(ExecutionStatus.FAILED, ExecutionStatus.valueOf("FAILED"));
-    }
-
-    @Test
+    @DisplayName("OntologyConstants should have valid values and private constructor")
     void testOntologyConstants() throws Exception {
-        assertNotNull(OntologyConstants.CLASS_PROMETHEUS_CONDITION);
-        assertNotNull(OntologyConstants.PROP_HAS_COMPENSATION);
-
-        // Test private constructor for coverage
-        Constructor<OntologyConstants> constructor = OntologyConstants.class.getDeclaredConstructor();
-        assertTrue(java.lang.reflect.Modifier.isPrivate(constructor.getModifiers()));
-        constructor.setAccessible(true);
-        constructor.newInstance();
+        assertAll(
+                () -> assertNotNull(OntologyConstants.CLASS_PROMETHEUS_CONDITION),
+                () -> assertNotNull(OntologyConstants.PROP_HAS_COMPENSATION),
+                () -> {
+                    // Test private constructor for coverage
+                    Constructor<OntologyConstants> constructor = OntologyConstants.class.getDeclaredConstructor();
+                    assertTrue(java.lang.reflect.Modifier.isPrivate(constructor.getModifiers()));
+                    constructor.setAccessible(true);
+                    assertNotNull(constructor.newInstance());
+                }
+        );
     }
 
     @Test
+    @DisplayName("ThemisProperties and its nested records should work correctly")
     void testThemisProperties() {
-        ThemisProperties.GraphDB graphdb = new ThemisProperties.GraphDB("url", "repo", 1000);
-        assertEquals("url", graphdb.url());
-        assertEquals("repo", graphdb.repositoryId());
-        assertEquals(1000, graphdb.timeoutMs());
-        assertNotNull(graphdb.toString());
-        assertEquals(new ThemisProperties.GraphDB("url", "repo", 1000), graphdb);
-        assertEquals(graphdb.hashCode(), new ThemisProperties.GraphDB("url", "repo", 1000).hashCode());
-
-        ThemisProperties.Ontology ontology = new ThemisProperties.Ontology("ns");
-        assertEquals("ns", ontology.moamNamespace());
-        assertNotNull(ontology.toString());
-        assertEquals(new ThemisProperties.Ontology("ns"), ontology);
-        assertEquals(ontology.hashCode(), new ThemisProperties.Ontology("ns").hashCode());
-
-        ThemisProperties.Prometheus prometheus = new ThemisProperties.Prometheus("purl");
-        assertEquals("purl", prometheus.url());
-        assertNotNull(prometheus.toString());
-        assertEquals(new ThemisProperties.Prometheus("purl"), prometheus);
-        assertEquals(prometheus.hashCode(), new ThemisProperties.Prometheus("purl").hashCode());
-
-        ThemisProperties props = new ThemisProperties(graphdb, ontology, prometheus);
-        assertEquals(graphdb, props.graphdb());
-        assertEquals(ontology, props.ontology());
-        assertEquals(prometheus, props.prometheus());
-        assertNotNull(props.toString());
-        assertEquals(props, new ThemisProperties(graphdb, ontology, prometheus));
-        assertEquals(props.hashCode(), new ThemisProperties(graphdb, ontology, prometheus).hashCode());
+        assertAll(
+                () -> {
+                    ThemisProperties.GraphDB graphdb = new ThemisProperties.GraphDB("url", "repo", 1000);
+                    assertAll(
+                            () -> assertEquals("url", graphdb.url()),
+                            () -> assertEquals("repo", graphdb.repositoryId()),
+                            () -> assertEquals(1000, graphdb.timeoutMs()),
+                            () -> assertNotNull(graphdb.toString()),
+                            () -> assertEquals(new ThemisProperties.GraphDB("url", "repo", 1000), graphdb),
+                            () -> assertEquals(graphdb.hashCode(), new ThemisProperties.GraphDB("url", "repo", 1000).hashCode())
+                    );
+                },
+                () -> {
+                    ThemisProperties.Ontology ontology = new ThemisProperties.Ontology("ns");
+                    assertAll(
+                            () -> assertEquals("ns", ontology.moamNamespace()),
+                            () -> assertNotNull(ontology.toString()),
+                            () -> assertEquals(new ThemisProperties.Ontology("ns"), ontology),
+                            () -> assertEquals(ontology.hashCode(), new ThemisProperties.Ontology("ns").hashCode())
+                    );
+                },
+                () -> {
+                    ThemisProperties.Prometheus prometheus = new ThemisProperties.Prometheus("purl");
+                    assertAll(
+                            () -> assertEquals("purl", prometheus.url()),
+                            () -> assertNotNull(prometheus.toString()),
+                            () -> assertEquals(new ThemisProperties.Prometheus("purl"), prometheus),
+                            () -> assertEquals(prometheus.hashCode(), new ThemisProperties.Prometheus("purl").hashCode())
+                    );
+                },
+                () -> {
+                    ThemisProperties.GraphDB graphdb = new ThemisProperties.GraphDB("url", "repo", 1000);
+                    ThemisProperties.Ontology ontology = new ThemisProperties.Ontology("ns");
+                    ThemisProperties.Prometheus prometheus = new ThemisProperties.Prometheus("purl");
+                    ThemisProperties props = new ThemisProperties(graphdb, ontology, prometheus);
+                    assertAll(
+                            () -> assertEquals(graphdb, props.graphdb()),
+                            () -> assertEquals(ontology, props.ontology()),
+                            () -> assertEquals(prometheus, props.prometheus()),
+                            () -> assertNotNull(props.toString()),
+                            () -> assertEquals(props, new ThemisProperties(graphdb, ontology, prometheus)),
+                            () -> assertEquals(props.hashCode(), new ThemisProperties(graphdb, ontology, prometheus).hashCode())
+                    );
+                }
+        );
     }
 }
--- modules/themis/src/test/java/com/kubiki/themis/knowledge/MoamMapperTest.java
+++ modules/themis/src/test/java/com/kubiki/themis/knowledge/MoamMapperTest.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MoaMapperTest {
+class MoamMapperTest {
 
     private MoamMapper mapper;
     private ValueFactory vf = SimpleValueFactory.getInstance();
