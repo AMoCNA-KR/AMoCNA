9bd828aee4996e1e4f990f1ce4a420b4212139f0
Zajdel Patryk <57092803+xMOROx@users.noreply.github.com>
2026-05-13 13:27:24 +0200

    fix: add missing ObjectMapper bean to support PrometheusConditionEvaluator

--- modules/themis/src/main/java/com/kubiki/themis/config/RabbitMQConfig.java
+++ modules/themis/src/main/java/com/kubiki/themis/config/RabbitMQConfig.java
@@ -1,5 +1,6 @@
 package com.kubiki.themis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -41,7 +42,12 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
