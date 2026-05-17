b805eead5c6c2aedc156fe615af2601849f0f905
Szymon Głomski <szymon.glomski@gmail.com>
2026-05-17 16:54:50 +0200

    Handle metis messages in palmedes

--- modules/palamedes/src/main/java/com/kubiki/palamedes/config/RabbitMQConfig.java
+++ modules/palamedes/src/main/java/com/kubiki/palamedes/config/RabbitMQConfig.java
@@ -12,9 +12,11 @@ public class RabbitMQConfig {
 
     public static final String ACTION_QUEUE = "amocna.action.queue";
     public static final String STATUS_QUEUE = "amocna.status.queue";
+    public static final String GRAPH_UPDATES_QUEUE = "amocna.graph.updates";
     public static final String EXCHANGE = "amocna.direct.exchange";
     public static final String ACTION_ROUTING_KEY = "action";
     public static final String STATUS_ROUTING_KEY = "status";
+    public static final String GRAPH_UPDATES_ROUTING_KEY = "graph.updates";
 
     @Bean
     public Queue actionQueue() {
@@ -24,6 +26,11 @@ public class RabbitMQConfig {
     @Bean
     public Queue statusQueue() {
         return new Queue(STATUS_QUEUE);
+    }
+
+    @Bean
+    public Queue graphUpdatesQueue() {
+        return new Queue(GRAPH_UPDATES_QUEUE, true);
     }
 
     @Bean
@@ -39,6 +46,11 @@ public class RabbitMQConfig {
     @Bean
     public Binding statusBinding(Queue statusQueue, DirectExchange exchange) {
         return BindingBuilder.bind(statusQueue).to(exchange).with(STATUS_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding graphUpdatesBinding(Queue graphUpdatesQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(graphUpdatesQueue).to(exchange).with(GRAPH_UPDATES_ROUTING_KEY);
     }
 
     @Bean
