e1a0de6857c644e5fe489143b072a349256ac7d5
Zajdel Patryk <57092803+xMOROx@users.noreply.github.com>
2026-05-20 14:15:47 +0200

    chore: fix issue with themis two objectmapper beans

--- apps/core/themis/src/main/java/com/kubiki/themis/config/BeanConfig.java
+++ apps/core/themis/src/main/java/com/kubiki/themis/config/BeanConfig.java
@@ -3,11 +3,13 @@ package com.kubiki.themis.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
+    @Primary
     public ObjectMapper getObjectMapper() {
         return new ObjectMapper();
     }
